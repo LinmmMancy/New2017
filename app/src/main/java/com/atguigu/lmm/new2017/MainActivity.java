2979a9a2b1b07a4b6e9495b2d36c4d99bae76171
LinmmMancy <519660797@qq.com>
2017-01-06 11:26:16 +0800

    32131231231

--- app/src/main/java/com/atguigu/lmm/new2017/MainActivity.java
+++ app/src/main/java/com/atguigu/lmm/new2017/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.print("111");
         System.out.print("111");
+        System.out.print("111");
 
 
     }
